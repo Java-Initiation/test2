ca07c12d8e13377002d34a969227a85fdc8131c3
Jérôme Clament-Sanz <j.clamentsanz@gmail.com>
2021-05-03 11:58:26 +0200

    first commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String familyName = "Dupont";
+
+        System.out.println("Hello mr " + familyName);
+
+
+    }
+
+}
